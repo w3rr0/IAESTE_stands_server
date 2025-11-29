cf9cf14796ab840db0369c4877df123e68093078
w3rr0 <konradmateja65@gmail.com>
2025-11-29 17:13:03 +0100

    add status endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,16 @@
-package server
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Ready")
+	})
+
+	log.Println("Server is running at :8080")
+	http.ListenAndServe(":8080", nil)
+}
