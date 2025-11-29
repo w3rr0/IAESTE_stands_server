880de6d7cdc7ccdfe98f50b27cb0b8fbe90de68c
w3rr0 <konradmateja65@gmail.com>
2025-11-29 17:48:52 +0100

    add user struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,23 @@
 package models
+
+type Name struct {
+	First string
+	Last  string
+}
+
+type Group string
+
+const (
+	IT      Group = "IT"
+	PR      Group = "PR"
+	IO      Group = "IO"
+	JFR     Group = "JFR"
+	Grafika Group = "Grafika"
+)
+
+type User struct {
+	ID    int8
+	Name  Name
+	Email string
+	Group Group
+}
