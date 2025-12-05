da6a6484be8d184927b309be8a09996d14f2102f
w3rr0 <konradmateja65@gmail.com>
2025-12-05 23:50:04 +0100

    add loading .env to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,17 @@
 package config
 
-var DatabaseURL = "postgres://postgres:password@localhost:5432/test?sslmode=disable"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("No .env file found, relying on system environment variables")
+	}
+}
+
+var DatabaseURL = os.Getenv("DATABASE_URL")
