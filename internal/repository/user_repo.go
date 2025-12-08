3957ef12950c5cbc19edfe73a18bb6243f484791
w3rr0 <konradmateja65@gmail.com>
2025-12-08 16:48:13 +0100

    add create user func with check account generate token and append account

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -36,3 +36,71 @@ func GetAllUsers(db *sql.DB) ([]models.User, error) {
 	}
 	return users, nil
 }
+
+func CreateUser(db *sql.DB, email string, password string) error {
+	exists, err := CheckAccount(db, email)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return errors.New("user already exists")
+	}
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	token, err := GenerateToken(32)
+	if err != nil {
+		return err
+	}
+
+	newAccount := models.Account{
+		Email:             email,
+		PasswordHash:      string(passwordHash),
+		VerificationToken: token,
+	}
+
+	err = AppendAccount(db, newAccount)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CheckAccount(db *sql.DB, email string) (bool, error) {
+	var exists bool
+
+	err := db.QueryRow(`
+		SELECT EXISTS(
+			SELECT 1
+			FROM users
+			WHERE email = $1
+	`, email).Scan(&exists)
+
+	return exists, err
+}
+
+func GenerateToken(length int) (string, error) {
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
+
+func AppendAccount(db *sql.DB, account models.Account) error {
+	_, err := db.Exec(`
+		INSERT INTO accounts
+			(email, password_hash, verification_token)
+		VALUES
+		    ($1, $2, $3)
+	`, account.Email, account.PasswordHash, account.VerificationToken)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
